6db5cfca2b835b87b59e3407c464758c2bd0fa88
mortaromarcello <mortaromarcello@gmail.com>
2012-09-13 07:03:46 +0200

    modified:   711-blobs.mk

--- 711-blobs.mk
+++ 711-blobs.mk
@@ -99,4 +99,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	${device_path}/releasetools/addon.d/70-gapps.sh:system/addon.d/70-gapps.sh \
 
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=GT-I9100 PRODUCT_BRAND=samsung BUILD_ID=IML74K BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.0.3/IML74K/BGLP8:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.0.3 IML74K BGLP8 release-keys" BUILD_NUMBER=BGLP8
 #end of 711-blobs.mk
