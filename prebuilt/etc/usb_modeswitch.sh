50503b255f9ea3518f0b256d766176f5d4c44b1d
mortaromarcello <mortaromarcello@gmail.com>
2012-12-03 18:47:32 +0100

    modified:   prebuilt/etc/3g_dongle.cfg 	modified:   prebuilt/etc/ppp/call-pppd 	modified:   prebuilt/etc/ppp/ip-down 	modified:   prebuilt/etc/ppp/ip-up 	modified:   prebuilt/etc/usb_modeswitch.sh 	modified:   ramdisk/init.sun4i.rc

--- prebuilt/etc/usb_modeswitch.sh
+++ prebuilt/etc/usb_modeswitch.sh
@@ -1,11 +1,30 @@
-#!/sbin/sh
-sleep 2
-#/system/bin/setprop "mode_switch_running" "1"
+#!/system/bin/busybox sh
 
-if [ -f $1 ]; then
-	/system/bin/usb_modeswitch -W -I -c $1
-else
-	echo "$1 does not exist."
+#################################################
+# usb serial device wrapper
+#################################################
+
+# check filename
+    
+if [ "$1" = "" ]; then
+    log "usb_dev_helper: device filename is NULL"
 fi
 
-#/system/bin/setprop "mode_switch_running" "0"
+# modem section
+if [ -f "$1" ]; then
+    PPP0_CONNECTED=`getprop net.ppp0.status`
+    PPP1_CONNECTED=`getprop net.ppp1.status`
+    if [ "$PPP0_CONNECTED" = "1" ] || [ "$PPP1_CONNECTED" = "1" ]; then
+	log "usb_dev_helper: ppp allready connected. skip usb_modeswitch call"
+    else
+        log "usb_dev_helper: start usb_modeswitch for $1"
+	/system/bin/usb_modeswitch -I -c "$1"
+    fi
+    exit 0;
+fi
+
+#################################################
+# future need add to this code for BT/GPS 
+# and other serial USB devices.
+#################################################
+log "usb_dev_helper: unknown model device and not supported"
